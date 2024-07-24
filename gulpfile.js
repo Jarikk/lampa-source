e55ab9a9fa39c5a69838fcfeb81980535a3024c9
Jarikk <Jarikk@gmail.com>
2024-07-24 21:56:05 +0300

    Updated build

--- gulpfile.js
+++ gulpfile.js
@@ -300,4 +300,5 @@ exports.test         = series(test);
 exports.default = parallel(watch, browser_sync);
 exports.debug = series(enable_debug_mode, this.default);
 exports.build = series(merge, plugins, sass_task, lang_task, sync_web, build_web);
+exports.pack_all = series(this.pack_webos, this.pack_tizen, this.pack_github, this.pack_plugins);
 
